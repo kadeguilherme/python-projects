e8241ccec009e2fa5cffa018f26f43ee11d0fe92
kadeguilherme <kadeguilherme@gmail.com>
2019-02-28 10:40:37 -0300

    Exercicios 24 do Gustavo Guanabara

---
+++ Exercicios-do-Guanabara/#023.py
@@ -0,0 +1,8 @@
+num = input("Informe um numero: ").zfill(4)
+
+print("\nAnalisando o número {}\n".format(num))
+
+print("Unidade {}".format(num[3]))
+print("Dezena {}".format(num[2]))
+print("Milha {}".format(num[1]))
+print("Centena{}".format(num[0]))
