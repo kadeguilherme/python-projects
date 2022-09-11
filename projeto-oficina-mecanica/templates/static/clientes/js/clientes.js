ab1b876252de3b7a53ea7989dade835e926816ba
kadeguilherme <kadeguilherme@gmail.com>
2022-09-11 14:09:02 -0300

    Conectando com BD SQLITE3

--- projeto-oficina-mecanica/templates/static/clientes/js/clientes.js
+++ projeto-oficina-mecanica/templates/static/clientes/js/clientes.js
@@ -1,10 +1,7 @@
 function add_carro(){
-
     container = document.getElementById('form-carro')
 
-    html = "<br> <div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro'></div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control'> </div></div>"
+    html = "<br>  <div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro' > </div><div class='col-md'><input type='text' placeholder='Placa' class='form-control' name='placa' > </div> </div>"
 
     container.innerHTML += html
-
-
 }
