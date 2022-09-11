6d803e3fef7a57c268914f71788ae170db27c92d
kadeguilherme <kadeguilherme@gmail.com>
2022-09-10 21:57:33 -0300

    Criando o html e conectando com django.

--- projeto-oficina-mecanica/mecajato/settings.py
+++ projeto-oficina-mecanica/mecajato/settings.py
@@ -7,6 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-i7mg-2bk$q60m6q2r6w%^bjeg)grs0dk8)lu&okey%%4^48++q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,7 +43,7 @@ ROOT_URLCONF = 'mecajato.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
