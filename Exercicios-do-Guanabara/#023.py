dbcc97d3b260bdbe45f3e41b751acea248bcf8ca
Guilherme <42500464+kadeguilherme@users.noreply.github.com>
2022-09-14 15:08:10 -0300

    Delete Exercicios-do-Guanabara directory

--- Exercicios-do-Guanabara/#023.py
+++
@@ -1,8 +0,0 @@
-num = input("Informe um numero: ").zfill(4)
-
-print("\nAnalisando o número {}\n".format(num))
-
-print("Unidade {}".format(num[3]))
-print("Dezena {}".format(num[2]))
-print("Milha {}".format(num[1]))
-print("Centena{}".format(num[0]))
