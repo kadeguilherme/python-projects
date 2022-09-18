3c0d7496315fdf6d32e60fea902b1245fee27a76
kadeguilherme <kadeguilherme@gmail.com>
2022-09-18 13:32:52 -0300

    Metodo GET criado

--- ong-bd/main.py
+++ ong-bd/main.py
@@ -2,7 +2,23 @@ from flask import Flask, jsonify, request
 import mysql.connector
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['JSON_SORT_KEYS'] = False
+
+db = mysql.connector.connect(
+    host ="localhost",
+    user="MainUser",
+    password = "MainPassword",
+    database="ONGS",
+)
+
+cursor = db.cursor()
+cursor.execute('SELECT * FROM ongs')
 
 
+@app.route("/query/ong", methods=['GET'])
+def get():
+    ong = cursor.fetchall()
+    return ong
 
 app.run()
