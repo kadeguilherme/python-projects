b0091e99704a6d73999d5ae3d11f0b6aa950e339
kadeguilherme <kadeguilherme@gmail.com>
2022-09-18 13:53:06 -0300

    Metodo POST criado

--- ong-bd/main.py
+++ ong-bd/main.py
@@ -13,12 +13,22 @@ db = mysql.connector.connect(
 )
 
 cursor = db.cursor()
-cursor.execute('SELECT * FROM ongs')
 
+sqlinsert = ("INSERT INTO ongs (founder,ongname,sector) VALUES (%s, %s, %s)") 
 
 @app.route("/query/ong", methods=['GET'])
 def get():
+    cursor.execute('SELECT * FROM ongs')
     ong = cursor.fetchall()
     return ong
+
+@app.route('/post/ong', methods=['POST'])
+def post():
+    ong = request.json
+    data_ong = (ong['founder'], ong['ongname'], ong['sector'])
+    cursor.execute(sqlinsert, data_ong)
+    db.commit()
+    return jsonify("Ong criada",ong)
+
 
 app.run()
